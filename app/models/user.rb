2d142d78a5bcbfbb06cb32ddcea4c997bf58e00f
K.Stepanov <unholy.vostok@gmail.com>
2016-07-31 02:21:19 +0300

    Add more validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
   before_create :create_remember_token
-  
+ 
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: {with: VALID_EMAIL_REGEX}
+  validates :nickname, presence: true, uniquenss: { case_sensetive: false }, length: { maximum: 20 }
+  validates :password, presence: true, on: :create
+  validates :password_confirmation, presence: true
+
   has_many :posts
   has_many :comments
 
