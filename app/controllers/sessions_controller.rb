1a37fad2da7f687f93ef3235e799decc8a455957
K.Stepanov <unholy.vostok@gmail.com>
2016-07-31 04:41:59 +0300

    Added Login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email]) 
-    if user && user.authenticate(params[:password])
-      login user
-      redirect_to posts_path 
+    user = User.find_by(email: params[:session][:email].downcase) 
+    if user && user.authenticate(params[:session][:password])
+      login user                                          
+      redirect_to user    
     else
       flash.now[:error] = "Invalid email/password combination."
       render :new
